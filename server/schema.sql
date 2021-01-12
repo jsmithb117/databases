d8602954c5de90d98a2f18f255ce7009c79f37bc
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-01-12 16:36:16 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,29 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  ID INT NOT NULL UNIQUE,
+  username TEXT NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE rooms (
+  ID INT NOT NULL UNIQUE,
+  roomname TEXT NOT NULL,
+  PRIMARY KEY (ID)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  ID INT NOT NULL UNIQUE,
+  message TEXT NOT NULL,
+  user INT NOT NULL,
+  room INT NOT NULL, 
+  created TEXT NOT NULL,
+  Primary KEY (ID),
+  FOREIGN KEY (user) REFERENCES users(ID),
+  FOREIGN KEY (room) REFERENCES rooms(ID)
 );
 
 /* Create other tables and define schemas for them here! */
